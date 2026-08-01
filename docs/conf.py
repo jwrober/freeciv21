a80129b5da3e2d62a90820f83e5c3f1e747964a4
James Robertson <jwrober@gmail.com>
2026-08-01 14:56:24 -0500

    Docs: update conf with our lua_ls backend

--- docs/conf.py
+++ docs/conf.py
@@ -251,4 +251,4 @@ if has_sphinx_lua_ls:
     # Path to the folder containing the `.luarc.json` file,
     # relative to the directory with `conf.py`.
     lua_ls_project_root = "../"
-
+    lua_ls_backend='luals'
